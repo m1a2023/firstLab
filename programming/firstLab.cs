307679e300ee0c30848ae9b9276d8934fde49828
qpwesuf <122185385+m1a2023@users.noreply.github.com>
2023-10-14 21:40:39 +0500

    Add files via upload

---
+++ programming/firstLab.cs
@@ -0,0 +1,51 @@
+namespace firstLab;
+
+class mathPerform
+{
+	static void Main(string[] args)
+	{
+		string inputedString = Console.ReadLine(); 
+		sortMath(inputedString);
+	}
+	
+	private static void sortMath(string input) //на вход принимается строка от пользователя
+	{
+		List<int> digits = new List<int>(); //ин-я списка чисел
+		List<char> symbols = new List<char>(); //ин-я списка матсимволов
+		
+        string clearInput = ""; //ин-я переменной, хранящая любые символы, кроме пробелов
+
+        for (int i = 0; i < input.Length; i++)
+            if (input[i] != ' ') { clearInput += input[i]; } //если в строке есть символ или цифра, добавлю его в clearInput
+
+        string timeDigitBox = ""; //временная переменная, хранящая цифры в числе
+
+		for (int i = 0;  i < clearInput.Length; i++)
+		{
+            string updateTime = "";
+
+			if (char.IsDigit(clearInput[i]))
+				timeDigitBox += clearInput[i]; //наполнил переменную цифрами
+
+			else
+			{
+				digits.Add(int.Parse(timeDigitBox)); // теперь нужно добавить числа в список
+				symbols.Add(clearInput[i]); //после того, как встретил неЧисло - добавил число в список
+                
+                timeDigitBox = updateTime; //обновил переменную после того, как встретил нежелаемый символ
+			}
+		} 
+		
+		if (!string.IsNullOrEmpty(timeDigitBox))
+        {
+            digits.Add(int.Parse(timeDigitBox));
+        }
+
+        //outPut
+        foreach ( int element in digits ) { Console.Write(element); } 
+        Console.WriteLine();
+		
+		foreach ( char element in symbols) { Console.Write(element); }
+        Console.WriteLine();
+	}
+}
